f6b43baf710732447cb202f2bd2ff2300d4e838a
williamjsmith15 <williamjsmith15@hotmail.co.uk>
2023-09-12 08:34:03 +0100

    Add geometry creator for testing the tools

---
+++ test_files/paramak_geometry_creator.py
@@ -0,0 +1,112 @@
+"""
+    This is a file to create an example geometry for the OpenMC workflow tool
+    The geometry is a simple reactor setup with a plasma, blanket, first wall and divertor
+
+    This can be run in the docker container williamjsmith15/omniverse-openmc:05062023 with the command:
+        docker run -it -v $PWD/:/home/ williamjsmith15/omniverse-openmc:05062023 /bin/bash
+    which will mount the current directory to the docker container and allow you to run this script with:
+        python paramak_geometry_creator.py <openmc_config.json>
+
+    where the json file is the one created in the openmc_json_creator.py file
+"""
+
+import paramak
+import numpy as np
+import argparse
+from stl_to_h5m import stl_to_h5m
+import json
+
+parser = argparse.ArgumentParser(
+    prog="paramak_generator.py", description="Paramak geometry generator"
+)
+
+parser.add_argument(
+    "config_file",
+    help="Config .json file with the parameters for the geometry creation. Format: JSON.",
+)
+args = parser.parse_args()
+config_path = args.config_file
+
+
+def parametric_blanket(config_dict):
+
+    plasma = paramak.Plasma(
+        minor_radius=config_dict["minor_radius"],
+        major_radius=config_dict["major_radius"],
+        triangularity=config_dict["triangularity"],
+        elongation=config_dict["elongation"],
+        rotation_angle=90,
+    )
+
+    firstwall = paramak.BlanketFP(
+        plasma=plasma,
+        thickness=config_dict["first_wall_thickness"],
+        stop_angle=230,
+        start_angle=-60,
+        offset_from_plasma=config_dict["plasma_offset"],
+        rotation_angle=90,
+        num_points=100,
+        color=list(
+            np.random.rand(3,)
+        ),
+    )
+
+    blanket_cut = paramak.BlanketFP(
+        plasma=plasma,
+        thickness=config_dict["blanket_thickness"]
+        + config_dict["first_wall_thickness"]
+        + 5,
+        stop_angle=230,
+        start_angle=-60,
+        offset_from_plasma=config_dict["plasma_offset"]
+        - config_dict["first_wall_thickness"],
+        rotation_angle=90,
+    )
+
+    blanket = paramak.BlanketFP(
+        plasma=plasma,
+        thickness=config_dict["blanket_thickness"],
+        stop_angle=230,
+        start_angle=-60,
+        offset_from_plasma=config_dict["plasma_offset"]
+        + config_dict["first_wall_thickness"],
+        rotation_angle=90,
+    )
+
+    divertor = paramak.BlanketFP(
+        plasma=plasma,
+        thickness=config_dict["blanket_thickness"],
+        stop_angle=270,
+        start_angle=-90,
+        offset_from_plasma=config_dict["plasma_offset"]
+        + config_dict["first_wall_thickness"],
+        rotation_angle=90,
+        cut=blanket_cut,
+        color=list(
+            np.random.rand(3,)
+        ),
+    )
+
+    divertor.export_stl("divertor.stl")
+    blanket.export_stl("blanket.stl")
+    firstwall.export_stl("firstwall.stl")
+    plasma.export_stl("plasma.stl")
+    reactor = paramak.Reactor([divertor, blanket, firstwall, plasma])
+
+    h5m_file = stl_to_h5m(
+        files_with_tags=[
+            ("divertor.stl", "divertor"),
+            ("blanket.stl", "blanket"),
+            ("firstwall.stl", "firstwall"),
+            ("plasma.stl", "plasma"),
+        ],
+        h5m_filename="dagmc.h5m",
+    )
+
+
+if __name__ == "__main__":
+    with open(config_path, "r") as f:
+        data = json.load(f)
+        config_dict = data["geometry"]
+
+    parametric_blanket(config_dict)
