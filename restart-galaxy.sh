9c126dcb492e0e6f1b8bd183ec8956f48e8d99ff
William Smith <williamjsmith15@hotmail.co.uk>
2023-11-16 12:02:02 +0000

    UNTESTED copying all tool folders

--- restart-galaxy.sh
+++ restart-galaxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker compose -f launch-galaxy.yml down
-sudo cp -rf ./galaxy-tools/example /galaxy/server/tools
+sudo cp -rf ./galaxy-tools/* /galaxy/server/tools
 docker compose -f launch-galaxy.yml up -d
